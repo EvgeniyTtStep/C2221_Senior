8d806fffe9beee0c324df8c76e4ca289f58f2d94
LukashykYevhenii <lukashyk.yevhenii@chnu.edu.ua>
2025-02-08 09:26:35 +0200

    lesson8

--- lesson8/myunittest.py
+++ lesson8/myunittest.py
@@ -1,5 +1,5 @@
 import unittest
-from lesson8 import adder
+from lesson8 import adder,sum
 
 class MyTest(unittest.TestCase):
 
@@ -8,3 +8,9 @@ class MyTest(unittest.TestCase):
 
     def test_args_negative(self):
         self.assertEqual(-6, adder(-2,-3))
+
+    def test_sum(self):
+        self.assertEqual(5, sum(2, 2))
+
+    def test_sum_zero(self):
+        self.assertEqual(1, sum(0, 0))
